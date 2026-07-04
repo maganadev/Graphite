f6627f1bca39b05b776f2c0692fb9e6302cfafca
maganadev <111192973+maganadev@users.noreply.github.com>
2026-07-04 22:39:06 +0900

    Try to get to compile

--- GDExtension/src/RegisterTypes.cpp
+++ GDExtension/src/RegisterTypes.cpp
@@ -28,7 +28,7 @@ void uninitialize_module(ModuleInitializationLevel p_level)
 extern "C"
 {
     // Initialization.
-    GDExtensionBool GDE_EXPORT libGDExtension_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization)
+    GDExtensionBool GDE_EXPORT libGraphiteGDExtensionInit(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization)
     {
         godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
