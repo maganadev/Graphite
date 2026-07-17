3a9cec840377e02aa21ef5c814e475184fbbc408
maganadev <111192973+maganadev@users.noreply.github.com>
2026-07-18 00:47:41 +0900

    Add CopyToCorrectLocation

---
+++ RepoScripts/CopyToCorrectLocation.bat
@@ -0,0 +1,4 @@
+copy /Y ..\godot\bin\godot.windows.template_debug.x86_64.exe         %APPDATA%\Godot\export_templates\4.5.3.rc\windows_debug_x86_64.exe
+copy /Y ..\godot\bin\godot.windows.template_debug.x86_64.console.exe %APPDATA%\Godot\export_templates\4.5.3.rc\windows_debug_x86_64_console.exe
+copy /Y ..\godot\bin\godot.windows.template_release.x86_64.exe         %APPDATA%\Godot\export_templates\4.5.3.rc\windows_release_x86_64.exe
+copy /Y ..\godot\bin\godot.windows.template_release.x86_64.console.exe %APPDATA%\Godot\export_templates\4.5.3.rc\windows_release_x86_64_console.exe
